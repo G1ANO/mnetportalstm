571fcee72511e99762b316f9b0637f98371018da
shantelle wambui <shantellewambuikungu@gmail.com>
2025-10-23 12:23:24 +0300

    add user route with password hashing

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify,request
 from flask_migrate import Migrate
-from models import db, bcrypt
+from models import db, bcrypt, User
 from config import Config
 
 app = Flask(__name__)
@@ -13,6 +13,32 @@ migrate = Migrate(app, db)
 @app.route('/')
 def home():
     return jsonify({"message": "Welcome to WiFi Portal"}), 200
+
+@app.route('/register', methods=['POST'])
+def register():
+    
+    data = request.get_json()
+
+    if not data.get('email') or not data.get('password'):
+        return jsonify({"error": "Email and password required"}), 400
+
+    if User.query.filter_by(email=data['email']).first():
+        return jsonify({"error": "Email already exists"}), 400
+    #this checks if email already exists
+    
+    user = User(
+        name=data.get('name'),
+        email=data['email'],
+        phone_number=data.get('phone_number'),
+        role='user'
+    )#this creates a new user
+
+    user.set_password(data['password'])
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify({"message": "User registered successfully"}), 201
+#this registers a new user and requires name,email,password and phonenumber 
 
 if __name__ == '__main__':
     app.run(debug=True)
