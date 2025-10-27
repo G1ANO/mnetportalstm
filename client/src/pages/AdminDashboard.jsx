1e1dc7a6e799d494b026e5c1d6889724005c3179
shantelle wambui <shantellewambuikungu@gmail.com>
2025-10-27 13:20:36 +0300

    add state and fetch function for tiers and complaints

--- client/src/pages/AdminDashboard.jsx
+++ client/src/pages/AdminDashboard.jsx
@@ -1,6 +1,28 @@
 import React from 'react';
+import axios from 'axios';
 
 const AdminDashboard = ({ user }) => {
+  const [tiers, setTiers] = useState([]);
+  const [complaints, setComplaints] = useState([]);
+
+  useEffect(() => {
+    fetchTiers();
+    fetchComplaints();
+  }, []);
+
+  const fetchTiers = () => {
+    axios.get('http://localhost:5000/tiers')
+      .then(res => setTiers(res.data))
+      .catch(err => console.error(err));
+  };
+
+  const fetchComplaints = () => {
+    axios.get(`http://localhost:5000/complaints?user_id=${user.id}`)
+      .then(res => setComplaints(res.data))
+      .catch(err => console.error(err));
+  };
+
+
   return (
     <div className="admin-dashboard">
       <h2>Admin Dashboard</h2>
